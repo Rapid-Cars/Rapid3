f7a930ed447a994066082cba65f526a41b0d752a
Robin <24611970+robmroi@users.noreply.github.com>
2024-11-27 10:25:30 +0100

    Implement i2c code to main file
    Implements the i2c communication from /Software/I2CSample to the main file.
    
    Improved the processCameraData function.

--- Software/Teensy/main/main.ino
+++ Software/Teensy/main/main.ino
@@ -1,6 +1,7 @@
 
 // The library Servo.h provides the functions for the motor and servo
 #include "Servo.h"
+#include <Wire.h>
 
 //Constants for this project
 //Motor / ESC
@@ -12,6 +13,15 @@ Servo esc;
 int servoPin; // The pin where the steering servo is attached
 Servo steeringServo;
 
+// Buffer for received Data
+#define BUFFER_SIZE 32
+char buffer[BUFFER_SIZE];
+
+int speed = 0;      // Speed of the motor
+int angle = 0;   // Steering angle of the servo
+int count = 0;
+int speeds[5];
+int angles[5];
 
 void setup() {
   // Init esc
@@ -22,11 +32,46 @@ void setup() {
   // Init servo
   servoPin = 1; //CHANGE BEFORE USE
   steeringServo.attach(servoPin);
+
+  // Init I2C
+  Wire.begin(0x12); // Teensy as Slave with adress 0x12
+  Wire.onReceive(receiveEvent); // Callback for received data
+  /*
+  Pinout:
+  P19: SCL
+  18: SDA
+  */
+  Serial.begin(115200);         // Debug-Output
 }
 
 void loop() {
+  // Process data from the camera
+  //processCameraData(speed, angle);
+  //delay(15);
+
+  // For testing
   testESC();
   testSteering();
+}
+
+// Processes the motor speed and steering angle.
+// Uses the last five values for each to build an average value.
+// The average value will then be sent to proper functions.
+void processCameraData(int speed, int angle) {
+  speeds[count] = speed;
+  angles[count] = angle;
+  count = count + 1;
+  if (count > 4) {
+    count = 0;
+    int averageSpeed = 0;
+    int averageAngle = 0;
+    for (int i = 0; i < 5; i++) {
+      averageSpeed += speeds[i];
+      averageAngle += angles[i];
+    }
+    setESCSpeed(averageSpeed);
+    setSteeringAngle(averageAngle);
+  }
 }
 
 // Sets the speed of the esc
@@ -45,6 +90,23 @@ void setSteeringAngle(int angle) {
   angle = constrain(angle, 0, 100);
   angle = map(angle, 0, 100, 0, 180); // Scales the angle to use it with the servo (value between 0 and 180)
   steeringServo.write(angle);
+}
+
+// Callback: receive data from Master (cam)
+void receiveEvent(int numBytes) {
+  int i = 0;
+  while (Wire.available() && i < BUFFER_SIZE - 1) {
+    buffer[i++] = Wire.read();
+  }
+  buffer[i] = '\0'; // Terminate string
+
+  // Process CSV Input
+  sscanf(buffer, "%d,%d", &speed, &angle);
+  // Output received Data
+  Serial.print("Empfangen - Speed: ");
+  Serial.print(speed);
+  Serial.print(", Steering angle: ");
+  Serial.println(angle);
 }
 
 // Tests the function of the motor
